34da8da83f350c47e9e1a8ccc9d1e4af1f890510
Evan Tatum <evantatum@hotmail.com>
2016-01-11 15:37:34 -0800

    console.log works need to edit the scripts for grammer

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is my README file.
 
 there are many like it but this one is mine.
+First edit/ added shops and first try at math 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,57 @@
-// // my js will be in here
-//
-//
-// QUACK QUACK QUACK 
+var coffeeMaster = function() {
+         this.coffeeShops = [];
+         this.addShop = function(shop){
+           this.coffeeShops.push(shop);
+         };
+         this.generateReport = function(){
+           for (var i = 0; i < this.coffeeShops.length; i++){
+             console.log("The " + this.coffeeShops[i].location + " location needs to have " + this.coffeeShops[i].getcoffeePerHour() + " coffees per hour, and " + this.coffeeShops[i].getcoffeePerDay() + " coffees per day.");
+           }
+         };
+       };
+
+var coffeeShop = function(location,min,max,avg,pound,hours) {
+    this.location = location;
+    this.min = min;
+    this.max = max;
+    this.avg = avg;
+    this.pound = pound;
+    this.hours = 14;
+    this.customersPerHour = function(){
+      return (Math.floor(Math.random() * (this.max - this.min + 1)) + this.min);
+         };
+    this.getcoffeePerHour = function(){
+      return Math.round((this.customersPerHour() * this.avg));
+         };
+     this.getcoffeePerDay = function(){
+           this.coffeePerDay = 0;
+           for (var i = 1; i <= this.hours; i++) {
+              this.coffeePerDay += this.getcoffeePerHour();
+            }
+            return this.coffeePerDay;
+          };
+        };
+var pikePlace = new coffeeShop ("Pike Place Market", 14, 55, 1.2, 3.7);
+var capitolHill = new coffeeShop ("Capitol Hill",32,48,3.2,0.4);
+var seattleLake = new coffeeShop ("Seattle Lake Union",35,88,1.3,3.7);
+var seattlePublic = new coffeeShop ("Seattle Public Library",49,75,2.6,0.2);
+var seaTac = new coffeeShop ("Sea-Tac Airport",68,124,1.1,2.7);
+
+              var coffeemas = new coffeeMaster();
+
+              coffeemas.addShop(pikePlace);
+              coffeemas.addShop(capitolHill);
+              coffeemas.addShop(seattleLake);
+              coffeemas.addShop(seattlePublic);
+              coffeemas.addShop(seaTac);
+              console.log ('why dont you work')
+
+              coffeemas.generateReport();
+
+              function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var header = table.createTHead();
+    var row = header.insertRow(0);
+    var cell = row.insertCell(0);
+    cell.innerHTML = "<b>Coffee Shop Data</b>";
+}
